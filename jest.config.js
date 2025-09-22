77f18b9ebafdb2ba77c66dacd784d5fe91c12b29
daxantz <daxantz@gmail.com>
2025-09-22 18:45:17 -0400

    added correct path for tests to config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.(test|spec).ts', '**/?(*.)+(test|spec).ts'],
 }
