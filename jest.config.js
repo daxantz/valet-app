b9ff4d922694beebe777434c4f912a5d123bfce6
daxantz <daxantz@gmail.com>
2025-09-22 18:41:02 -0400

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { createDefaultPreset } = require('ts-jest/presets')
-
-const tsJestTransformCfg = createDefaultPreset().transform
-
-/** @type {import("jest").Config} */
-const config = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    ...tsJestTransformCfg,
-  },
-  testMatch: ['**/__tests__/**/*.test.ts'], // look for tests in __tests__
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // optional setup (Prisma, DB, etc.)
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  clearMocks: true, // auto-clear mocks between tests
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  roots: ['<rootDir>/src'],
 }
-
-module.exports = config
