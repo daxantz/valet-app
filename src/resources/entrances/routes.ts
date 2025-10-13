b4060b5ba3e21b535674778dc26c204f8ef667e1
daxantz <daxantz@gmail.com>
2025-10-13 18:24:37 -0400

    create route for handling car actions

--- src/resources/entrances/routes.ts
+++ src/resources/entrances/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import entranceController from './controller'
-
+import carRouter from '../cars/routes'
 const router = Router({ mergeParams: true })
 
 // define routes
@@ -9,4 +9,5 @@ router.delete('/:id', entranceController.deleteEntrance)
 router.get('/', entranceController.getEntrancesByLocation)
 router.get('/:id', entranceController.getSingleEntrance)
 router.put('/:id', entranceController.updateEntrance)
+router.use('/:entranceId/car', carRouter)
 export default router
