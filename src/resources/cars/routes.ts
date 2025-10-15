db12c8776ac261115a59038cf2afe8105c122ff8
daxantz <daxantz@gmail.com>
2025-10-15 18:11:53 -0400

    create endpoint that adds multer images to request

--- src/resources/cars/routes.ts
+++ src/resources/cars/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import carController from './controller'
+import { upload } from '../../services/aws'
 
 const router = Router({ mergeParams: true })
 
 // define routes
 router.get('/', carController.getCars)
-// router.get('/', carController.getCars)
+router.post('/', upload.array('images', 4), carController.createCar)
 router.delete('/:id', carController.removeCar)
 // router.get('/', entranceController.getEntrancesByLocation)
 router.get('/:id', carController.getSingleCar)
